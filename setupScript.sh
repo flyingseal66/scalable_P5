6a25f85135b5604e08fb5af469181cc790b5e8dc
murchant <mamurtag@tcd.ie>
2018-10-31 15:21:29 +0000

    Driver and setup script

---
+++ setupScript.sh
@@ -0,0 +1,8 @@
+sudo apt-get install linux-headers-`uname -r`
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install build-essential -y
+sudo apt-get install linux-image-extra-virtual -y
+sudo /bin/bash NVIDIA-Linux-x86_64-410.72.run
+sudo apt install hashcat -y
+hashcat -I
